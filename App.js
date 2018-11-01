a8634781bdcf9dfdb0cae759642ed77af3f278b5
Grzegorz Kielak <grzegorz@kielak.me>
2018-11-01 21:27:24 +0100

    First version

--- App.js
+++ App.js
@@ -1,21 +1,75 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, TouchableNativeFeedback, View} from 'react-native';
 
 export default class App extends React.Component {
+  interval = null;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      owner: null,
+      scores: [0, 0],
+    };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.updateScore(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start hacking!</Text>
-      </View>
+    const pauseButton = null != this.state.owner ? (
+        <TouchableNativeFeedback onPress={() => this.setState({owner: null})}>
+          <View style={{
+            position: 'absolute',
+            top: '45%',
+            left: '40%',
+            backgroundColor: '#000',
+          }}>
+            <Text style={{color: '#FFF', fontSize: 50}}>
+              Stop
+            </Text>
+          </View>
+        </TouchableNativeFeedback>
+    ) : null;
+
+    const teamButton = (owner, backgroundColor) => (
+        <TouchableNativeFeedback onPress={() => this.setState({owner})}>
+          <View style={{
+            backgroundColor,
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+            <Text style={{color: '#FFF', fontSize: 100}}>
+              {this.state.scores[owner]}
+            </Text>
+          </View>
+        </TouchableNativeFeedback>
     );
+
+    return (
+        <View style={{flex: 1}}>
+          {teamButton(0, 'blue')}
+          {teamButton(1, 'red')}
+          {pauseButton}
+        </View>
+    );
+  }
+
+  updateScore() {
+    this.setState((state) => {
+      if (null == state.owner) {
+        return state;
+      }
+  const scores = [...state.scores];
+      scores[state.owner]++;
+
+      return {...state, scores};
+    });
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
